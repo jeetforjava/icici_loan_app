7a6a1e8da61ee92341dea8acd3d89d1982e69c65
jeet <jeetforjava@gmail.com>
2023-05-24 13:26:03 +0530

    new file added

--- src/main/java/icici_loan/Loan_Account.java
+++ src/main/java/icici_loan/Loan_Account.java
@@ -5,5 +5,16 @@ public class Loan_Account {
 		public static void main(String[] args) {
 			System.out.println("Housing loan account");
 		}
+<<<<<<< Updated upstream
 		int principal;
+=======
+		int InterestCal(int I,int P, int N, int R) {
+			I=P*N*R/100;
+			return I; 
+		}
+		int Loan_Due(int I,int P) {
+			int P1=P-I;
+			return P1; 
+		}
+>>>>>>> Stashed changes
 }
